3a5f5247a59d4674ade484ed2d5f5db34172d39a
Francis Bonacic <francisbonacic@icloud.com>
2021-01-26 14:55:57 -0800

    finished setting up form/login state

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,11 +1,11 @@
-
 import './App.css';
+import {LoginForm} from './components/LoginForm';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello, friends!</h1>
-   
+      <LoginForm />
     </div>
   );
 }
---
+++ friends/src/components/LoginForm.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import axios from 'axios';
+
+export class LoginForm extends React.Component{
+
+
+    state = {
+    credentials:{
+    username:"",
+    password:""
+    }};
+
+    handleChange = (e) => {
+    this.setState({
+        credentials:{
+            ...this.state.credentials,
+            [e.target.name]: e.target.value
+        }
+    });
+    };
+
+    login = (e) => {
+        e.preventDefault();
+        axios.post('http://localhost:5000/api/login',this.state.credentials)
+            .then(res => {
+                // console.log(res)
+                localStorage.setItem('token', res.data.payload);
+                // this.props.history.push('/protected');
+            })
+            .catch(err => {
+                console.log(err);
+            })
+    }
+
+
+render(){
+return(
+<>
+<h2>Friend Form:</h2>
+<form onSubmit={this.login}>
+    <input
+       type="text"
+       name="username"
+       value={this.state.credentials.username}
+       onChange={this.handleChange}
+       placeholder="username:"
+    />
+    <br/>
+    <input
+       type="password"
+       name="password"
+       value={this.state.credentials.password}
+       onChange={this.handleChange}
+       placeholder="password:"
+    />
+    <br/>
+    <br/>
+    <button>login</button>
+</form>
+</>
+)
+}
+}
+
